8378f76f540d6b9ac49f91c367534794450e66f8
mollemaproducts <mollemaproducts@gmail.com>
2025-02-08 16:43:50 +0100

    First test client

--- code/utilities/bybit_client.py
+++ code/utilities/bybit_client.py
@@ -2,8 +2,8 @@ import ccxt
 
 class BybitClient():
 
-    def __init__(self, authentication, use_testenvironment=true) -> None
+    def __init__(self, authentication, use_test_environment=True) -> None:
         self.session = ccxt.bybit(authentication)
-        self.session.set_sandbox_mode(use_testenvironment)
+        self.session.set_sandbox_mode(use_test_environment)
 
 
