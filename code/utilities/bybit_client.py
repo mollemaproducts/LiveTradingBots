eb16e4260e50a6c8e9cb931549fbe69e3319be15
mollemaproducts <mollemaproducts@gmail.com>
2025-02-08 16:57:22 +0100

    First test client

--- code/utilities/bybit_client.py
+++ code/utilities/bybit_client.py
@@ -6,7 +6,17 @@ class BybitClient():
     # Function to initialize the Bybit client
     def __init__(self, authentication, use_test_environment=True) -> None:
         self.session = ccxt.bybit(authentication)
-        self.session.set_sandbox_mode(use_test_environment)
+        self.session.set_sandbox_mode(True)
+        balance = self.session.fetch_balance(params={'defaultType': 'future'})
+        btc_balance = balance['BTC']
+        usdt_balance = balance['USDT']
+
+        # Print balances
+        #print(Fore.GREEN + "BALANCE" + Style.RESET_ALL)
+        print(f"Balance BTC : {btc_balance}")
+        print(f"Balance USDT: {usdt_balance}")
+        print()
+
 
     # Function to fetch and display balances
     def get_balance(self):
