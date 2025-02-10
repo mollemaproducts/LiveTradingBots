a80200eea91e6f0554a16a7860371aadcae51ee1
mollemaproducts <mollemaproducts@gmail.com>
2025-02-10 22:07:33 +0100

    update of ada with original code with new setup

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ sudo apt install python3-pip -y
 echo Making log dirs
 mkdir logs
 
+chmod 755 ./code/run_envelope.sh
+
 echo Installing virtual environment and packages
 cd code
 sudo apt-get install python3-venv
