448df04f8861d7170522a45b021f0a8bd5d99909
mollemaproducts <mollemaproducts@gmail.com>
2025-02-12 22:47:49 +0100

    moved strategy code to strategy logic old and new strategy logic (create with chatgpt) to strategy logic The code still is using the old version.

---
+++ code/utilities/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+
+class Logger:
+
+    @staticmethod
+    def init(log_path, coin) -> None:
+        # Initialize logging
+        log_file = os.path.join(log_path, "bot_" + coin.lower() + ".log")
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.FileHandler(log_file),  # Log to file
+                logging.StreamHandler()  # Log to console as well
+            ]
+        )
