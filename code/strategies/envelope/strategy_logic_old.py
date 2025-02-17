ca335e1c9792f22bda449e668df488afa4d40bb6
mollemaproducts <mollemaproducts@gmail.com>
2025-02-17 19:49:41 +0100

    Update fo the old strategy_logic_old.py

--- code/strategies/envelope/strategy_logic_old.py
+++ code/strategies/envelope/strategy_logic_old.py
@@ -44,10 +44,18 @@ class StrategyLogic:
       #  logging.info(f"{datetime.now().strftime('%H:%M:%S')}: ohlcv data fetched")
 
         # --- DETERMINE TRIGGER PRICES ---
+        print(data.columns)
+        print(data.tail())
         for i in range(len(self.params['envelopes'])):
             print(f"Processing envelope {i + 1} with value {i}")
-            data[f'entry_trigger_high_{i + 1}'] = data[f'band_high_{i + 1}'].iloc[-1] * (1 - self.TRIGGER_PRICE_DELTA)
-            data[f'entry_trigger_low_{i + 1}'] = data[f'band_low_{i + 1}'].iloc[-1] * (1 + self.TRIGGER_PRICE_DELTA)
+            if f'band_high_{i + 1}' in data.columns:
+                data[f'entry_trigger_high_{i + 1}'] = data[f'band_high_{i + 1}'].iloc[-1] * (1 - self.TRIGGER_PRICE_DELTA)
+            else:
+                print(f"Column 'band_high_{i + 1}' not found!")
+            if f'band_low_{i + 1}' in data.columns:
+                data[f'entry_trigger_low_{i + 1}'] = data[f'band_low_{i + 1}'].iloc[-1] * (1 + self.TRIGGER_PRICE_DELTA)
+            else:
+                print(f"Column 'band_low_{i + 1}' not found!")
         logging.info(f"{datetime.now().strftime('%H:%M:%S')}: trigger prices calculated")
         print("After adding bands, columns:", data.columns)
 
