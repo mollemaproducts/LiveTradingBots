831540bf000e6cdfe8cf3cce5574bf4b6315b1ef
mollemaproducts <mollemaproducts@gmail.com>
2025-02-17 19:44:33 +0100

    Update fo the old strategy_logic_old.py

--- code/strategies/envelope/strategy_logic_old.py
+++ code/strategies/envelope/strategy_logic_old.py
@@ -44,10 +44,12 @@ class StrategyLogic:
       #  logging.info(f"{datetime.now().strftime('%H:%M:%S')}: ohlcv data fetched")
 
         # --- DETERMINE TRIGGER PRICES ---
-      #  for i in range(len(self.params['envelopes'])):
-      #      data[f'entry_trigger_high_{i + 1}'] = data[f'band_high_{i + 1}'].iloc[-1] * (1 - self.TRIGGER_PRICE_DELTA)
-      #      data[f'entry_trigger_low_{i + 1}'] = data[f'band_low_{i + 1}'].iloc[-1] * (1 + self.TRIGGER_PRICE_DELTA)
-      #  logging.info(f"{datetime.now().strftime('%H:%M:%S')}: trigger prices calculated")
+        for i in range(len(self.params['envelopes'])):
+            print(f"Processing envelope {i + 1} with value {i}")
+            data[f'entry_trigger_high_{i + 1}'] = data[f'band_high_{i + 1}'].iloc[-1] * (1 - self.TRIGGER_PRICE_DELTA)
+            data[f'entry_trigger_low_{i + 1}'] = data[f'band_low_{i + 1}'].iloc[-1] * (1 + self.TRIGGER_PRICE_DELTA)
+        logging.info(f"{datetime.now().strftime('%H:%M:%S')}: trigger prices calculated")
+        print("After adding bands, columns:", data.columns)
 
 
 
